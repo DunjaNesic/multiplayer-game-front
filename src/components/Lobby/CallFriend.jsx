8b501bced39a7146966dd376aa26bf935a8626a4
DunjaNesic <dn20200113@student.fon.bg.ac.rs>
2023-08-19 17:54:01 +0200

    Fixed CallFriend

--- src/components/Lobby/CallFriend.jsx
+++ src/components/Lobby/CallFriend.jsx
@@ -1,57 +1,68 @@
-import React from 'react'
-import './user.css'
+import React, { useState, useEffect } from 'react';
+import './user.css';
 import io from 'socket.io-client';
 
 function CallFriend() {
+  const [roomCode, setRoomCode] = useState('');
+  const [socket, setSocket] = useState(null);
 
-  const [roomCode, setRoomCode]=React.useState("");
-  const [socket, setSocket] = React.useState(null);
-
-    const clickedButton = () => {  
-      let result = '';
-      const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'; 
-      let counter = 0;
-      while (counter < characters.length) {
-        result += characters.charAt(Math.floor(Math.random() * characters.length));
-        counter += 1;
-      }
-      setRoomCode(result);
+  const clickedButton = () => {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let counter = 0;
+    while (counter < characters.length) {
+      result += characters.charAt(Math.floor(Math.random() * characters.length));
+      counter += 1;
     }
+    setRoomCode(result);
+  };
 
+  const handleSocketConnection = () => {
     if (!socket) {
+      console.log('soket');
       const newSocket = io('http://localhost:3000');
       setSocket(newSocket);
+      console.log("nultisoket");
 
       newSocket.on('connect', () => {
         console.log('Socket connected');
-        newSocket.emit('sendRoomCode', roomCode); 
+        if (roomCode.trim() !== '') {
+          console.log('soket2');
+          newSocket.emit('sendRoomCode', roomCode);
+        } else {
+          console.log('Room code is empty. Please generate a code first.');
+        }
       });
     } else {
-      socket.emit('sendRoomCode', roomCode);
-    }
-
-    React.useEffect(() => {
-      if (socket) {
-        // Ovde dobijam odgovor od Ive
-        socket.on('roomCodeSent', (response) => {
-          console.log('Room code sent to server:', response);
-          // Ovde definisem sta ce da se desi na mojoj strani kad mi Iva odgovori
-          console.log("dunja");
-        });
+      if (roomCode.trim() !== '') {
+        console.log('soket3');
+        socket.emit('sendRoomCode', roomCode);
+      } else {
+        console.log('Room code is empty. Please generate a code first.');
       }
-    }, [socket]);
+    }
+  };
+
+  useEffect(() => {
+    if (socket) {
+      socket.on('gameStart', (response) => {
+        console.log('Room code sent to server:', response);
+        console.log('dunja'); 
+      });
+    }
+  }, [socket]);
 
   return (
     <div className="invitation">
       <div className="call">
-    <div className='invite--oponent'>Invite Your Oponent Through Room Code</div>
-    <p><i className="arrow right"></i></p>
-    <button className='roomBtn' onClick={clickedButton}>Get Room Code</button>
+        <div className='invite--oponent'>Invite Your Opponent Through Room Code</div>
+        <p><i className="arrow right"></i></p>
+        <button className='roomBtn' onClick={clickedButton}>Get Room Code</button>
+      </div>
+      <p className='code'>Input this code to start the game: {roomCode}</p>
+      <input type="text" onChange={handleSocketConnection} />
     </div>
-    <p className='code'>Send This Code To Your Friend</p>
-    <input type="text" value={roomCode} />
-  </div>
-  )
+  );
 }
 
-export default CallFriend
+export default CallFriend;
